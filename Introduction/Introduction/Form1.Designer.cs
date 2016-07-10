bb43104b5de56204843837df1f67481cb1a5e1a6
ZoneLearning <myselflearningzone@gmail.com>
2016-07-10 20:29:27 +0300

    .ıntroduction -->select ve insert işlemleri entity framework ile yapıldı.

--- Introduction/Introduction/Form1.Designer.cs
+++ Introduction/Introduction/Form1.Designer.cs
@@ -29,7 +29,20 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbSupplier = new System.Windows.Forms.ComboBox();
+            this.nudPrice = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nudStock = new System.Windows.Forms.NumericUpDown();
+            this.btnAdd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStock)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -37,26 +50,144 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(13, 57);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(413, 264);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(546, 264);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün adı";
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(16, 30);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtProductName.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kategori";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(123, 30);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategory.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tedarikçi";
+            // 
+            // cmbSupplier
+            // 
+            this.cmbSupplier.FormattingEnabled = true;
+            this.cmbSupplier.Location = new System.Drawing.Point(250, 30);
+            this.cmbSupplier.Name = "cmbSupplier";
+            this.cmbSupplier.Size = new System.Drawing.Size(121, 21);
+            this.cmbSupplier.TabIndex = 2;
+            // 
+            // nudPrice
+            // 
+            this.nudPrice.Location = new System.Drawing.Point(377, 30);
+            this.nudPrice.Name = "nudPrice";
+            this.nudPrice.Size = new System.Drawing.Size(73, 20);
+            this.nudPrice.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(377, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Fiyat";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Stok";
+            // 
+            // nudStock
+            // 
+            this.nudStock.Location = new System.Drawing.Point(456, 30);
+            this.nudStock.Name = "nudStock";
+            this.nudStock.Size = new System.Drawing.Size(73, 20);
+            this.nudStock.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(535, 27);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 333);
+            this.ClientSize = new System.Drawing.Size(622, 333);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.nudStock);
+            this.Controls.Add(this.nudPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbSupplier);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudStock)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbSupplier;
+        private System.Windows.Forms.NumericUpDown nudPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown nudStock;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- Introduction/Introduction/Form1.cs
+++ Introduction/Introduction/Form1.cs
@@ -24,7 +24,32 @@ namespace Introduction
             NorthWindDataContext context = new NorthWindDataContext();
 
             dataGridView1.DataSource = context.Products;
+            cmbCategory.DataSource = context.Categories;
+            cmbCategory.DisplayMember = "CategoryName";
+            cmbCategory.ValueMember = "CategoryID";
+            cmbSupplier.DataSource = context.Suppliers;
+            cmbSupplier.DisplayMember = "CompanyName";
+            cmbSupplier.ValueMember = "SupplierID";
 
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            NorthWindDataContext context = new NorthWindDataContext();
+            Product product = new Product();
+            product.ProductName = txtProductName.Text;
+            product.UnitPrice = nudPrice.Value;
+            product.UnitsInStock = (short)nudStock.Value;
+            product.CategoryID = (int)cmbCategory.SelectedValue;
+            product.SupplierID = (int)cmbSupplier.SelectedValue;
+
+            context.Products.InsertOnSubmit(product);
+            /*Linq to sql data contexti yapılan değişiklikleri(insert,update,delete) doğrudan SQL koduna dönüştürmez. 
+                Bunu yapmak için bir komut bekler.Şuana kadar yaptığın işlemleri database'e aktar demek için SubmitChanges() demek gerekir.
+            Bunu diyene kadar DB değişmez.*/
+            context.SubmitChanges();
+
+            dataGridView1.DataSource = context.Products;
         }
     }
 }
