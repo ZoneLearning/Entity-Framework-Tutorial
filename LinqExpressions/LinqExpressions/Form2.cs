cfb0266d868c734ec0996eff04812f357b4cb6a8
ZoneLearning <myselflearningzone@gmail.com>
2016-07-11 12:08:11 +0300

    LinqExpression  -> farklı tablolar aynı kolon isimlerine sahipse ,anonymous tip içerisnde hata verir. Bu yüzden değişken ismi veririz.

---
+++ LinqExpressions/LinqExpressions/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqExpressions
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            NorthWindDataContext context = new NorthWindDataContext();
+            dataGridView1.DataSource = from od in context.Order_Details
+                                       join p in context.Products
+                                       on od.ProductID equals p.ProductID
+                                       select new
+                                       {
+                                           p.ProductName,
+                                           p.UnitPrice,
+                                           OrderPrice = od.UnitPrice,
+                                           od.Quantity,
+                                           od.Discount
+
+                                       };
+
+        }
+    }
+}
