9f5ec07991f39ea89050dd6458470825d26a1ff8
ZoneLearning <myselflearningzone@gmail.com>
2016-07-11 14:18:34 +0300

    LinqExpressions --> order by ile sıralama (descending/ascending)

---
+++ LinqExpressions/LinqExpressions/Form5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinqExpressions
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+        NorthWindDataContext context = new NorthWindDataContext();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var result = from product in context.Products
+                         orderby product.UnitPrice
+                         select product;
+            dataGridView1.DataSource = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var result = from product in context.Products
+                         orderby product.ProductName descending  // Z'den A'ya dogru
+                         select product;
+            dataGridView1.DataSource = result;
+        }
+    }
+}
