307567d6de7573139ef4af5c3aa300999bead1a6
ZoneLearning <myselflearningzone@gmail.com>
2016-07-12 09:45:07 +0300

    RepositoryPattern --> Repository ile alınan nesne, DTO'ya dönüştürülüp gönderildi.

---
+++ RepositoryPattern/RepositoryPattern.DTO/Product/ListProductDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.DTO.Product
+{
+    // Sadece ürün için bir sürü DTO olabilir. DTO içine Product isimili klasör açıyoruz. Her DTO için klasör açılabilir.
+
+    public class ListProductDTO
+    {
+        // Kolaylık olsun diye ORM'den Product ile ilgili property'leri alıyorum.
+
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public Nullable<int> SupplierID { get; set; }
+        public Nullable<int> CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public Nullable<decimal> UnitPrice { get; set; }
+        public Nullable<short> UnitsInStock { get; set; }
+        public Nullable<short> UnitsOnOrder { get; set; }
+        public Nullable<short> ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+    }
+}
---
+++ RepositoryPattern/RepositoryPattern.Service/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using RepositoryPattern.DTO.Product;
+using RepositoryPattern.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Service
+{
+    public class ProductService
+    {
+        //Arayüzden nelere ihtiyaç duyuluyorsa
+        //DTO'nun geldiği ve repository'e iletilmesi gereken istekler
+        //listeleme, ekleme, silme, güncelleme istegi gelebilir.
+
+        //ürün listelenirken hangi alanların gideceğini belirlemek lazım. Ve servis sınfında nasıl olacagını DTO'da belirlemek lazım.
+
+        ProductRepository repository = new ProductRepository();
+        public IList<ListProductDTO> getList() // DTO nesnesine dönüştürerek gönderdik.
+        {
+            return repository.getList().Select(   // repositor'den normal nesneyi edip
+                x => new ListProductDTO
+                {
+                    ProductName = x.ProductName,
+                    UnitPrice = x.UnitPrice,
+                    UnitsInStock = x.UnitsInStock,
+                    ProductID = x.ProductID,
+                    ReorderLevel = x.ReorderLevel,
+                    QuantityPerUnit = x.QuantityPerUnit,
+                    CategoryID=x.CategoryID,
+                    SupplierID = x.SupplierID,
+                    Discontinued=x.Discontinued,
+                    UnitsOnOrder = x.UnitsOnOrder
+                }
+                ).ToList();
+        }
+
+    }
+}
