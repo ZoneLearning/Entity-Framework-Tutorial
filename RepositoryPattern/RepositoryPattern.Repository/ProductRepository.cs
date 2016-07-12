d6359594b31ce45d468a6af13b7dff13f72d94c2
ZoneLearning <myselflearningzone@gmail.com>
2016-07-12 09:05:41 +0300

    RepositoryPattern -->  ProductRepository'i  RepositoryBase'den miras alınarak olusturuldu.

---
+++ RepositoryPattern/RepositoryPattern.Repository/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryPattern.ORM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Repository
+{
+    public class ProductRepository : RepositoryBase<Product>
+    {
+        //insert,select,update,delete işlemleri base class'da var. Burayı Product ile ilgili extra sorgular için kullanıyoruz.
+        public void Barter(Product gotton, Product given) // takas metodu
+        {
+
+        }
+    }
+}
